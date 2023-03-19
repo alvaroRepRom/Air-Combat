6b847ae6b9e2523f8b36a7376d404e73919ec37e
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-03-19 13:16:16 +0100

    Update ac_ranking.cpp

--- src/ac_ranking.cpp
+++ src/ac_ranking.cpp
@@ -30,7 +30,7 @@ namespace ac
         _bg(bn::regular_bg_items::sky.create_bg(0, 0)),
         _game_events(game_events),
         _text_generator(common::variable_8x8_sprite_font),
-        _current_letter(0),
+        _current_letter(65),
         _is_start_frame(true),
         _save_data_ranking(Save_Ranking::load_score()),
         _new_record_position(player_result_position(_game_events->score, _save_data_ranking.score_array))
@@ -42,53 +42,95 @@ namespace ac
     {
         bn::optional<Scene_Type> result;
 
-        if (_is_start_frame) 
-        {
-            int height = -40;
-            int score_index_offset = 0;
-
-            for (int score_index = 0; score_index < ac::constants::NUMBER_SAVES_SCORE; score_index++)
-            {
-                bn::string<32> text;        
-                bn::ostringstream text_stream(text);
-
-                if (score_index == _new_record_position) {
-                    // insert name
-                    text_stream.append(":  ");
-                    text_stream.append(_game_events->score);
-                    score_index_offset = 1;
-                }
-                else {
-                    for (int i = 0; i < ac::constants::NUMBER_NAME_LETTERS; i++) {
-                        text_stream.append(_save_data_ranking.name_array[score_index - score_index_offset][i]);                    
-                    }
-                    text_stream.append(":  ");
-                    text_stream.append(_save_data_ranking.score_array[score_index - score_index_offset]);
-                }
-                
-                _text_generator.generate(0, height, text, _text_sprites_array[score_index]);
-                height += 20;
-            }
-
-            _is_start_frame = false;
+        if (bn::keypad::up_pressed()) {
+            _current_letter += 1;
+            if (_current_letter > 90)
+                _current_letter = 65;
         }
-        else
+        else 
+        if (bn::keypad::down_pressed()) {
+            _current_letter -= 1;
+            if (_current_letter < 65)
+                _current_letter = 90;
+        }
+
+
+        int height = -40;
+        int score_index_offset = 0;
+
+        for (int score_index = 0; score_index < ac::constants::NUMBER_SAVES_SCORE; score_index++)
         {
-            if (bn::keypad::up_pressed()) {
-                _current_letter += 1;
-                if (_current_letter > 24)
-                    _current_letter = 0;
+            bn::string<32> text;        
+            bn::ostringstream text_stream(text);
+
+            if (score_index == _new_record_position) {
+                // insert name
+                text_stream.append(char(_current_letter));
+                text_stream.append(":  ");
+                text_stream.append(_game_events->score);
+                score_index_offset = 1;
             }
-            else 
-            if (bn::keypad::down_pressed()) {
-                _current_letter -= 1;
-                if (_current_letter < 0)
-                    _current_letter = 24;
+            else {
+                for (int i = 0; i < ac::constants::NUMBER_NAME_LETTERS; i++) {
+                    text_stream.append(_save_data_ranking.name_array[score_index - score_index_offset][i]);                    
+                }
+                text_stream.append(":  ");
+                text_stream.append(_save_data_ranking.score_array[score_index - score_index_offset]);
             }
 
-            if (bn::keypad::start_pressed()) {
-                result = Scene_Type::INTRO;
-            }
+            _text_sprites_array[score_index].clear();
+            _text_generator.generate(0, height, text, _text_sprites_array[score_index]);
+            height += 20;
+        }
+
+        // if (_is_start_frame) 
+        // {
+        //     int height = -40;
+        //     int score_index_offset = 0;
+
+        //     for (int score_index = 0; score_index < ac::constants::NUMBER_SAVES_SCORE; score_index++)
+        //     {
+        //         bn::string<32> text;        
+        //         bn::ostringstream text_stream(text);
+
+        //         if (score_index == _new_record_position) {
+        //             // insert name
+        //             text_stream.append(":  ");
+        //             text_stream.append(_game_events->score);
+        //             score_index_offset = 1;
+        //         }
+        //         else {
+        //             for (int i = 0; i < ac::constants::NUMBER_NAME_LETTERS; i++) {
+        //                 text_stream.append(_save_data_ranking.name_array[score_index - score_index_offset][i]);                    
+        //             }
+        //             text_stream.append(":  ");
+        //             text_stream.append(_save_data_ranking.score_array[score_index - score_index_offset]);
+        //         }
+                
+        //         _text_generator.generate(0, height, text, _text_sprites_array[score_index]);
+        //         height += 20;
+        //     }
+
+        //     _is_start_frame = false;
+        // }
+        // else
+        // {
+        //     if (bn::keypad::up_pressed()) {
+        //         _current_letter += 1;
+        //         if (_current_letter > 24)
+        //             _current_letter = 0;
+        //     }
+        //     else 
+        //     if (bn::keypad::down_pressed()) {
+        //         _current_letter -= 1;
+        //         if (_current_letter < 0)
+        //             _current_letter = 24;
+        //     }
+
+        // }
+
+        if (bn::keypad::start_pressed()) {
+            result = Scene_Type::INTRO;
         }
 
         return result;
