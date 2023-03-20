87dc4be8f44b5a3b59b83a1294c7a5177879cdc6
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-03-20 13:09:02 +0100

    intro updated

--- include/ac_intro.h
+++ include/ac_intro.h
@@ -1,9 +1,13 @@
 #ifndef AC_INTRO_H
 #define AC_INTRO_H
 
+// butano
+#include "bn_affine_bg_ptr.h"
+// air combat
 #include "ac_scene_type.h"
 #include "ac_scene.h"
-#include "bn_affine_bg_ptr.h"
+// arr
+#include "arr_timer.h"
 
 namespace ac
 {
@@ -18,7 +22,7 @@ namespace ac
 
         private:
             bn::affine_bg_ptr _bg;
-            int _frames_to_wait;
+            arr::Timer _timer;
     };
 }
 
--- src/ac_intro.cpp
+++ src/ac_intro.cpp
@@ -9,23 +9,21 @@
 
 namespace ac
 {
-    namespace
-    {
+    namespace {
         constexpr int NUM_FRAMES = constants::FPS * 3; // 3 seconds
     }
 
     Intro::Intro() : 
         _bg(bn::affine_bg_items::guilty_d_logo.create_bg(0, 0)),
-        _frames_to_wait(NUM_FRAMES)
+        _timer(NUM_FRAMES)
     {}
 
     bn::optional<Scene_Type> Intro::update()
     {
         bn::optional<Scene_Type> result;
-        
-        _frames_to_wait--;
-        if (_frames_to_wait == NUM_FRAMES / 2) _bg = bn::affine_bg_items::butano_background.create_bg(0, 0);
-        if (!_frames_to_wait) result = Scene_Type::TITLE;
+
+        if (_timer.is_time_up()) result = Scene_Type::TITLE;
+        if (_timer.frames_elapsed() == NUM_FRAMES / 2) _bg = bn::affine_bg_items::butano_background.create_bg(0, 0);
 
         return result;
     }
