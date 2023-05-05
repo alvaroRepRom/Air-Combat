aa19bf1aebec994dd0d5c5adde5dc04e8e324467
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-05-05 12:57:10 +0200

    Title Scene Art and Logic

--- include/ac_title.h
+++ include/ac_title.h
@@ -8,6 +8,8 @@
 #include "ac_scene_type.h"
 #include "ac_scene.h"
 #include "ac_game_events.h"
+// arr
+#include "arr_timer.h"
 
 namespace ac
 {
@@ -23,8 +25,11 @@ namespace ac
         private:
             bn::regular_bg_ptr _bg;
             bn::sprite_ptr _sprite;
+            bn::sprite_ptr _arrow_select;
+            bool _is_right_arrow;
             int _palette_index;
             Game_Events* _game_events;
+            arr::Timer _arrow_timer;
     };
 }
 
--- src/ac_title.cpp
+++ src/ac_title.cpp
@@ -4,7 +4,8 @@
 #include "bn_array.h"
 #include "bn_sprite_palette_ptr.h"
 // assets
-#include "bn_regular_bg_items_sky.h"
+#include "bn_regular_bg_items_title.h"
+#include "bn_sprite_items_arrow.h"
 #include "bn_sprite_items_plane_sheet.h"
 #include "bn_sprite_palette_items_pink_palette.h"
 #include "bn_sprite_palette_items_red_palette.h"
@@ -20,15 +21,26 @@ namespace ac
     }
 
     Title::Title(Game_Events* game_events) : 
-        _bg(bn::regular_bg_items::sky.create_bg(0, 0)),
-        _sprite(bn::sprite_items::plane_sheet.create_sprite(0, 0)),
+        _bg(bn::regular_bg_items::title.create_bg(0, 0)),
+        _sprite(bn::sprite_items::plane_sheet.create_sprite(0, 16)),
+        _arrow_select(bn::sprite_items::arrow.create_sprite(72, 16)),
+        _is_right_arrow(true),
         _palette_index(0),
-        _game_events(game_events)
-    {}
+        _game_events(game_events),
+        _arrow_timer(30)
+    {
+        _arrow_select.set_visible(false);
+    }
 
     bn::optional<Scene_Type> Title::update()
     {
         bn::optional<Scene_Type> result;
+
+        if (_arrow_select.visible()) {
+            if (_arrow_timer.is_time_up()) {
+                _arrow_select.set_visible(false);
+            }
+        }
 
         if (bn::keypad::left_pressed()) {
             _palette_index--;
@@ -36,6 +48,10 @@ namespace ac
                 _palette_index = palette_array.size() - 1;
 
             _sprite.set_palette(palette_array[_palette_index]);
+
+            _arrow_select.set_visible(true);
+            _arrow_select.set_x(-72);
+            _arrow_select.set_horizontal_flip(true);
         } 
         else 
         if (bn::keypad::right_pressed()) {
@@ -44,6 +60,10 @@ namespace ac
                 _palette_index = 0;
 
             _sprite.set_palette(palette_array[_palette_index]);
+
+            _arrow_select.set_visible(true);
+            _arrow_select.set_x(72);
+            _arrow_select.set_horizontal_flip(false);
         }
         
         if (bn::keypad::start_pressed()) {
